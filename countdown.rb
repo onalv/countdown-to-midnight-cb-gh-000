f730ccfa7c5a01a5fb777a626f7b7b43357e8ab8
Omar Nava <omarnavaalvarez@gmail.com>
2018-03-13 02:31:24 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -8,4 +8,11 @@ def countdown(number)
   return "HAPPY NEW YEAR!"
 end
 
-# countdown(10)
+def countdown_with_sleep(number)
+  while number > 0
+    puts "#{number} SECOND(S)!"
+    number -= 1
+    sleep(1)
+  end
+  return "HAPPY NEW YEAR!"
+end
